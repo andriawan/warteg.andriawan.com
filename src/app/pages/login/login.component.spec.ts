c2d17d4064d4da371510a9466c31c62d2884d457
Muhammad Irwan Andriawan <irwan@andriawan.com>
2024-02-21 04:39:24 +0000

    feat(login): add base component login page

---
+++ src/app/pages/login/login.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LoginComponent } from './login.component';
+import { RouterTestingModule } from '@angular/router/testing';
+
+describe('LoginComponent', () => {
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [LoginComponent, RouterTestingModule]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(LoginComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
