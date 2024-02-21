1c08b41a2652586a6bdb0fc895845278515c36b3
Muhammad Irwan Andriawan <irwan@andriawan.com>
2024-02-21 10:44:13 +0000

    feat(login): add form group

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,47 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [
+    FlexLayoutModule,
+    ReactiveFormsModule,
+    RouterLink,
+    RouterLinkActive,
+    MatCardModule,
+    MatGridListModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  onLogin() {
+    console.log(this.form);
+  }
+  passwordVisibility: boolean = false;
+  form: FormGroup = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [
+      Validators.minLength(6),
+      Validators.required,
+    ]),
+  });
+}
