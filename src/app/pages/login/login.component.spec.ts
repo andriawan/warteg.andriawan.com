7ede2072ababbc16a99c80a0731112d451354a15
Muhammad Irwan Andriawan <irwan@andriawan.com>
2024-02-21 10:45:53 +0000

    test(login): add test to login component

--- src/app/pages/login/login.component.spec.ts
+++ src/app/pages/login/login.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -9,7 +10,7 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LoginComponent, RouterTestingModule],
+      imports: [LoginComponent, RouterTestingModule, NoopAnimationsModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(LoginComponent);
@@ -19,5 +20,23 @@ describe('LoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('test invalid login form', () => {
+    const formData = {
+      username: '',
+      password: '',
+    };
+    component.form.setValue(formData);
+    expect(component.form.invalid).toEqual(true);
+  });
+
+  it('test valid login form', () => {
+    const formData = {
+      username: 'examplere',
+      password: '12345678',
+    };
+    component.form.setValue(formData);
+    expect(component.form.valid).toEqual(true);
   });
 });
