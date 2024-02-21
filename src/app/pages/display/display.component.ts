54c8ad892babaa76b1edc7dbb72ca28d5c9dbb75
Muhammad Irwan Andriawan <irwan@andriawan.com>
2024-02-21 13:34:58 +0000

    feat(display): add base display component page

---
+++ src/app/pages/display/display.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-display',
+  standalone: true,
+  imports: [],
+  templateUrl: './display.component.html',
+  styleUrl: './display.component.css',
+})
+export class DisplayComponent {}
