f82644ac5344f0c41d2a5ff1afb7073e0bb8e879
Muhammad Irwan Andriawan <irwan@andriawan.com>
2024-02-21 04:39:49 +0000

    feat(landing): add base component landing page

---
+++ src/app/pages/landing/landing.component.spec.ts
@@ -0,0 +1,36 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LandingComponent } from './landing.component';
+
+describe('LandingComponent', () => {
+  let component: LandingComponent;
+  let fixture: ComponentFixture<LandingComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [LandingComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(LandingComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it(`should have the 'warteg.andriawan.com' title`, () => {
+    const fixture = TestBed.createComponent(LandingComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('warteg.andriawan.com');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(LandingComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, warteg.andriawan.com');
+  });
+});
---
+++ src/app/pages/landing/landing.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-landing',
+  standalone: true,
+  imports: [RouterLinkActive, RouterLink],
+  templateUrl: './landing.component.html',
+  styleUrl: './landing.component.css'
+})
+export class LandingComponent {
+  title = 'warteg.andriawan.com';
+}
