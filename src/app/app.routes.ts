0a8ffa9dbb2116e93b5766779872c4aeadd80a49
Muhammad Irwan Andriawan <irwan@andriawan.com>
2024-02-21 05:25:42 +0000

    feat(route): add route for page login and landing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { LandingComponent } from './pages/landing/landing.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "login", component: LoginComponent },
+    { path: "", component: LandingComponent}
+];
