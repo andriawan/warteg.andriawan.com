260b09a7267c1a642d432965ba24011aac24cb92
Muhammad Irwan Andriawan <irwan@andriawan.com>
2024-02-23 07:29:04 +0000

    refactor(item-interface): add id and counter

--- src/app/interfaces/item.ts
+++ src/app/interfaces/item.ts
@@ -1,8 +1,10 @@
 import { ItemCategory } from '../enums/item-category';
 
 export default interface Item {
+  id: string | number;
   name: string;
   price: number;
+  counter?: number;
   active: boolean;
   catgegory: ItemCategory.DRINK | ItemCategory.FOOD;
   image: string;
