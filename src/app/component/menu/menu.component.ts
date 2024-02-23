ad3bab80e8e09913e69328c35c6da7fdcbf708fd
Muhammad Irwan Andriawan <irwan@andriawan.com>
2024-02-23 07:27:17 +0000

    refactor(menu): add event emitter when toggle select menu

--- src/app/component/menu/menu.component.ts
+++ src/app/component/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import Item from '../../interfaces/item';
@@ -14,11 +14,13 @@ import { CurrencyPipe } from '@angular/common';
 export class MenuComponent {
   @Input() data: Item | undefined;
   @Input() indexItem: number = 0;
+  @Output() selectedItem: EventEmitter<Item> = new EventEmitter<Item>();
 
   toggleSelectionItem() {
     if (this.data) {
       this.data.index = this.indexItem;
       this.data.selected = !this.data.selected;
+      this.selectedItem.emit(this.data);
     }
   }
 }
