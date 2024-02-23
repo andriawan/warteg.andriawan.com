a11cc0402b8c613840efbfb327b2f83ec8c20e09
Muhammad Irwan Andriawan <irwan@andriawan.com>
2024-02-23 01:49:46 +0000

    refactor(style): refactor data and UI main page display

--- src/app/pages/display/display.component.ts
+++ src/app/pages/display/display.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { AuthService } from '../../auth.service';
 import { Router } from '@angular/router';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MenuComponent } from '../../component/menu/menu.component';
+import Item from '../../interfaces/item';
+import data from '../../sample/items';
 
 @Component({
   selector: 'app-display',
   standalone: true,
-  imports: [MatGridListModule],
+  imports: [MatGridListModule, MenuComponent],
   templateUrl: './display.component.html',
   styleUrl: './display.component.css',
 })
@@ -15,6 +18,8 @@ export class DisplayComponent {
     private authService: AuthService,
     private router: Router
   ) {}
+
+  data: Item[] = data;
 
   logout() {
     this.authService.logout().subscribe(() => {
