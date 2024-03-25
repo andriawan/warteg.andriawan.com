a7e4b1c5402ff7a3dd823da0f3e0d0a5c08d0a27
HeL <aqhelaja@gmail.com>
2024-03-25 23:10:10 +0700

    update: server side -> add server.dev.proxy.js ( js mode ) for proxy-ing api development to ng serve

---
+++ server.dev.proxy.js
@@ -0,0 +1,19 @@
+
+module.exports = {
+  '/api': {
+    secure: false,
+    changeOrigin:true,
+    cookieDomainRewrite: "localhost:4200",
+    target: "http://localhost:4000" ,
+
+    /** @type {import('express').Handler } */
+    bypass: ( req , res ) => {
+      // console.log('DIMANA INI')
+      // // req.set
+      // console.log(req.headers)
+      // console.log(req.cookies)
+      
+      res.setHeader('X-Header-Test', 'Test-Modify-Header-Response');
+    },
+  }
+}
