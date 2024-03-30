d856d0554cef35b6f9ec95af219e65c44ec0c040
HeL <aqhelaja@gmail.com>
2024-03-30 23:30:03 +0700

    add environment for prod

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,18 @@
+
+export const environment = {
+  production: true,
+  isMockEnabled: false,
+  appName: 'Prod : Warteg Mania',
+  appVersion: 'v0.1',
+  USERDATA_KEY: 'warteg',
+  ApiUrlClient: `/api`,
+  appKey:'',
+  port: 0 ,
+  ApiUrlServer: () => {
+    return `http://localhost${ environment.port ? `:${environment.port}` : '' }/api`
+  },
+}
+
+environment.appKey = `${environment.USERDATA_KEY}:${environment.appVersion}`
+// environment.ApiUrlServer += `:${ process.env.PORT || 4000 }/api`
+environment.USERDATA_KEY += `_${environment.appVersion}`
