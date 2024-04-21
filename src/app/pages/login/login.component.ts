1d35d0adeba9a52778af2103bc9c8cd92f02e417
HeL <aqhelaja@gmail.com>
2024-04-22 01:24:30 +0700

    update: login page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
@@ -6,21 +6,29 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+
 import {
   FormControl,
   FormGroup,
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { AuthService } from '../../auth.service';
-import { ROLES } from '../../enums/roles';
+// import { FlexLayoutModule } from '@angular/flex-layout';
+// import {FlexLayoutServerModule} from '@angular/flex-layout/server'
+import { AuthService } from '../../core/services/auth.service';
+// import { ROLES } from '../../enums/roles';
+import { Api, MyAssets, clientLoading , handleError } from '@client/core/utils/helpers';
+import { ENDPOINTS } from '@environments/endpoints';
+import { Subject, map, switchMap, takeUntil } from 'rxjs';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    FlexLayoutModule,
+    // FlexLayoutModule,
+    // FlexLayoutServerModule,
     ReactiveFormsModule,
     RouterLink,
     RouterLinkActive,
@@ -30,27 +38,84 @@ import { ROLES } from '../../enums/roles';
     MatIconModule,
     MatButtonModule,
     MatFormFieldModule,
+    MatProgressBarModule,
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit , OnDestroy {
+  private readonly unsubscribe$: Subject<any> = new Subject()
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next(null)
+    this.unsubscribe$.complete()
+  }
+
   constructor(
     private authService: AuthService,
-    private router: Router
+    private router: Router ,
   ) {}
-  onLogin() {
-    this.authService.saveToken('sample');
-    this.authService.saveRole(ROLES.CASHIER);
-    this.authService.getToken().subscribe(() => {
-      this.router.navigateByUrl('/display');
-    });
+
+  ngOnInit(): void {
+    
   }
+  
+  isLoading = false
+
+   async onLogin() {
+
+    try{
+      await MyAssets().isLoaded()
+      
+      const params = this.form.value
+      const setLoader = ( value = true ) =>{
+        value ? clientLoading.show() : clientLoading.close()
+        this.isLoading = value
+      }
+      
+      setLoader(true)
+      // delayClient(1000).then(()=>this.router.navigateByUrl('/'))
+
+      Api().post( ENDPOINTS.auth.login , params ).pipe(
+        takeUntil(this.unsubscribe$) ,
+        switchMap( (loginData : My.Object ) =>{
+          return Api().get( ENDPOINTS.auth.info ).pipe(
+            map( (userInfo : My.Object ) => ({ userInfo , loginData }))
+          )
+        }) ,
+        switchMap( response =>{
+          console.log(response )
+          const loginData = response.loginData?.data || {} 
+          const userInfo = response.userInfo?.data || {}
+
+          return this.authService.saveAuthPersistent$({
+            authToken: loginData.token ,
+            loginData, userInfo
+          })
+        })
+      ).subscribe({
+        next: response =>{
+          setLoader(false)
+          this.router.navigateByUrl('/dashboard')
+        },
+        error: response => {
+          handleError(response)
+          setLoader(false)
+        } ,
+      })
+
+    }catch(error){
+      
+      console.error(error)
+    }
+    
+  }
+
   passwordVisibility: boolean = false;
   form: FormGroup = new FormGroup({
     username: new FormControl('', [Validators.required]),
     password: new FormControl('', [
-      Validators.minLength(6),
+      Validators.minLength(1),
       Validators.required,
     ]),
   });
