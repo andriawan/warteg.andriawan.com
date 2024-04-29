c32f79ce08f86ecb281478ba624f9cd939002350
HeL <aqhelaja@gmail.com>
2024-04-30 01:30:33 +0700

    update: app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,33 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
+// import { LoginComponent } from './pages/login/login.component';
 import { LandingComponent } from './pages/landing/landing.component';
-import { DisplayComponent } from './pages/display/display.component';
-import { loggedInGuard } from './guard/logged-in.guard';
+// import { DisplayComponent } from './pages/display/display.component';
+import { isLoggedInGuard } from './guard/logged-in.guard';
+import { tokenGuard } from './guard/token.guard';
 
 export const routes: Routes = [
   {
     path: 'login',
-    component: LoginComponent,
+    // component: LoginComponent,
+    loadComponent: () => import('./pages/login/login.component').then( c => c.LoginComponent ),
     title: 'Login',
-    canActivate: [loggedInGuard],
+    canActivate: [isLoggedInGuard],
   },
-  { path: '', component: LandingComponent, title: 'WARTEG MANIA' },
   {
     path: 'display',
-    component: DisplayComponent,
+    // component: DisplayComponent,
+    loadComponent: () => import('./pages/display/display.component').then( c => c.DisplayComponent ),
     title: 'Display',
   },
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./pages/dashboard/layout/layout.component').then( c => c.LayoutComponent ),
+    canActivate:[ tokenGuard ],
+    children:[
+      { path: '' , loadChildren: ()=> import('./pages/dashboard/routes') }
+    ],
+    title: 'Dashboard',
+  },
+  { path: '', component: LandingComponent, title: 'WARTEG MANIA' , pathMatch:"full" },
+  { path: '**', redirectTo: '/' },
 ];
