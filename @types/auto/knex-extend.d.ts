b1663ffa7e90983f061e62330ac93a6bf6d8d6d2
HeL <aqhelaja@gmail.com>
2024-05-27 01:17:36 +0700

    update:@type for db.d.ts

--- @types/auto/knex-extend.d.ts
+++ @types/auto/knex-extend.d.ts
@@ -3,8 +3,11 @@ import { Knex } from 'knex'
 declare module 'knex/types/tables' {      
   interface Tables {
     'logs.session': DB.LogSession;
+    'transactions.orders': DB.TransactionOrder;
+    'mappings.transactions_orders___items': DB.MapOrderItem;
     users: DB.User;
     items: DB.Item;
+    files: DB.File;
     
     users_composite: Knex.CompositeTableType<
     DB.RawData,
