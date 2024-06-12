bd06d2c54593f076e5d731b880f67a4dd4b92042
HeL <aqhelaja@gmail.com>
2024-06-13 01:35:08 +0700

    update: environment for development

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,11 @@
 
+/**
+ * This file is used by Server Side ( api ) and Client Side ( angular )
+ * Make sure do not import any kind of library that only work on client side or server side
+ * 
+ * ATTENTION: Do not put any sensitive information here since this file will be bundled in client side as well
+ * If you need to set any sensitive key example: secret key , please use .env file
+*/
 export const environment = {
   production: false,
   isMockEnabled: false,
@@ -11,6 +18,7 @@ export const environment = {
   ApiUrlServer: () => {
     return `http://localhost${ environment.port ? `:${environment.port}` : '' }/api`
   },
+  cookieName: 'myAngularApp',
 }
 
 environment.appKey = `${environment.USERDATA_KEY}:${environment.appVersion}`
